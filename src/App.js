0e08671659b3640ed89af4874115d2b91a2c2e2a
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-04 04:12:29 +0530

    Build navBar component and configure routes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+/*
+`
+npm i react-router-dom
+`
+First we wrap App with BrowserRouter
+
+*/
+
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./component/movies";
+import MovieForm from "./component/movieForm";
+import Customers from "./component/customers";
+import Rentals from "./component/rentals";
+import NotFound from "./component/notFound";
+import NavBar from "./component/navBar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="Container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
