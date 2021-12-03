f888cf6f35a5b1dd2852ab629164a39b53d6537a
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-04 00:27:28 +0530

    Build the pagination common component.

--- src/component/movies.jsx
+++ src/component/movies.jsx
@@ -1,11 +1,15 @@
-// Add Conditional Rendering
+// Add Type Checking
 import React, { Component } from "react";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 import { getMovies } from "../services/fakeMovieService";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -22,10 +26,17 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
     if (count === 0) return <p>There are no movies in database.</p>;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
 
     //tabel.table>thead>tr>th*4
     return (
@@ -43,7 +54,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((movie) => (
+            {movies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
@@ -67,6 +78,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
