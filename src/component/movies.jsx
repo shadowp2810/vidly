d8e685b1ba9f911b3abd5cdbce311f18be613c18
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-03 23:02:16 +0530

    Add delete functionality to movies component.

--- src/component/movies.jsx
+++ src/component/movies.jsx
@@ -1,3 +1,4 @@
+// Add Delete functionality
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 
@@ -6,7 +7,14 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
+  handleDelete = (movie) => {
+    //gets all movies except the one passed here
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
   render() {
+    //tabel.table>thead>tr>th*4
     return (
       <tabel className="table">
         <thead>
@@ -15,15 +23,24 @@ class Movies extends Component {
             <th>Stock</th>
             <th>Genre</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map((movie) => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
