d27cf1aeecbc651fc43c4b647b3853d75f9a0cb5
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-06 04:54:39 +0530

    Refractor movieService

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,24 +3,30 @@ import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/movies";
 
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
-export function getMovie(movieID) {
-  return http.get(apiEndpoint + "/" + movieID);
+
+export function getMovie(movieId) {
+  return http.get(movieUrl(movieId));
 }
+
 export function saveMovie(movie) {
   //update existing movie
   if (movie._id) {
     //we dont want to directly modify movie so we clone it.
     const body = { ...movie };
     delete body._id;
-    return http.put(apiEndpoint + "/" + movie._id, body);
+    return http.put(movieUrl(movie._id), body);
   }
   //create new movie
   return http.post(apiEndpoint, movie);
 }
 
-export function deleteMovie(movieID) {
-  return http.delete(apiEndpoint + "/" + movieID);
+export function deleteMovie(movieId) {
+  return http.delete(movieUrl(movieId));
 }
